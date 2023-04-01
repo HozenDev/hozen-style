61810a967cfb2b234349fae75e3e75cde03a23d8
Hozen <enzo.durel@gmail.com>
2023-04-01 22:26:23 +0200

    optimize scroll and resize js

--- scripts/toc.js
+++ scripts/toc.js
@@ -4,9 +4,7 @@ window.addEventListener('load', function () {
     const toc = document.getElementById("table-of-contents");
     let header = document.createElement("header");
     let main = document.createElement("main");
-
-    tocAndContentToMain();
-    createHeader();
+    let textOfToc = document.getElementById('text-table-of-contents');
 
     function createHeader() {
 	header.appendChild(document.getElementsByClassName("title")[0]);
@@ -19,23 +17,8 @@ window.addEventListener('load', function () {
 	main.appendChild(content);
     }
 
-    function tocFollowContent() {
-	let marginLeftOfContent = getComputedStyle(content, null).marginLeft;
-	marginLeftOfContent = parseInt(marginLeftOfContent, 10);
-
-	let widthToc = getComputedStyle(toc, null).width;
-	widthToc = parseInt(widthToc, 10);
-	
-	if (marginLeftOfContent > widthToc) {
-	    content.style.margin = "0 auto";
-	    toc.style.left = `${marginLeftOfContent - widthToc + 2}px`;
-	}
-    }
-
-    function tocTopResize() {
-	let heightTitle = getComputedStyle(document.getElementsByClassName("title")[0], null).height;
-	toc.style.marginTop = heightTitle;
-    }
+    tocAndContentToMain();
+    createHeader();
 
     /* Media Query Toc */
 
@@ -57,44 +40,54 @@ window.addEventListener('load', function () {
 	    }
 	});
     }
+
+    // Returns a function, that, as long as it continues to be invoked, will not
+    // be triggered. The function will be called after it stops being called for
+    // N milliseconds. If `immediate` is passed, trigger the function on the
+    // leading edge, instead of the trailing.
+    function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+	    var context = this, args = arguments;
+	    var later = function() {
+		timeout = null;
+		if (!immediate) func.apply(context, args);
+	    };
+	    var callNow = immediate && !timeout;
+	    clearTimeout(timeout);
+	    timeout = setTimeout(later, wait);
+	    if (callNow) func.apply(context, args);
+	};
+    };
     
+    /* listener to know if we need to look window resize for toc visibility */
+    let resizeListener = false;
+
+    /* show toc for resize if user end by hide it */
+    let showToc = debounce(function() {
+        if (resizeListener && window.innerWidth >= 840) {
+            textOfToc.style.visibility = "visible";
+            resizeListener = false;
+            window.removeEventListener('resize', showToc);
+        }
+    }, 250);
+    
+    /* hide text of toc is click on h2 */
     toc.getElementsByTagName('h2')[0].addEventListener('click', (e) => {
-	if (!window.matchMedia("(min-width: 840px)").matches) {
-	    let textOfToc = document.getElementById('text-table-of-contents');
+        if (!window.matchMedia("(min-width: 840px)").matches) {
 	    if (getComputedStyle(textOfToc, null).visibility === 'hidden') {
-		textOfToc.style.visibility = "visible";
-		let rect = toc.getBoundingClientRect();
-		textOfToc.style.top = `${parseInt(rect.y, 10)+parseInt(rect.height, 10)}px`;
+                if (!resizeListener) {
+                    window.addEventListener('resize', showToc);
+                    resizeListener = true;
+                }
+                let h2 = toc.getElementsByTagName('h2')[0];
+                textOfToc.style.visibility = "visible";
+                textOfToc.style.top = `${h2.offsetHeight}px`;
 	    }
 	    else {
 		textOfToc.style.visibility = "hidden";
 	    }
 	}
-    });
-
-    /* Evenement de défilement pour resize la toc quand visible sur petits écrans */
-
-    window.addEventListener('scroll', (e) => {
-	if (!window.matchMedia("(min-width: 840px)").matches) {
-	    let textOfToc = document.getElementById('text-table-of-contents');
-	    if (getComputedStyle(textOfToc, null).visibility === 'visible') {
-		let rect = toc.getBoundingClientRect();
-		textOfToc.style.top = `${parseInt(rect.y, 10)+parseInt(rect.height, 10)}px`;
-	    }
-	}
-    });
-
-    window.addEventListener('resize', (e) => {
-	let textOfToc = document.getElementById('text-table-of-contents');
-	if (window.matchMedia("(min-width: 840px)").matches) {
-	    if (getComputedStyle(textOfToc, null).visibility === 'hidden') {
-		textOfToc.style.visibility = "visible";
-	    }
-	}
-	else {
-	    if (getComputedStyle(textOfToc, null).visibility === 'visible') {
-		textOfToc.style.visibility = "hidden";
-	    }
-	}
+        
     });
 });
